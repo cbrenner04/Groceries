bfc2eb59f02f796bf93c4d3fa10eae08639de7d1
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-02-11 19:15:55 -0600

    Fix Devise...again

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   resources :lists
   resources :items, only: [:new, :create, :edit, :update, :destroy]
   resources :users_lists, only: [:new, :create]
