3967bf38333cbc9f393549e706b0c6519a564876
cbrenner04 <cbrenner265@gmail.com>
2017-06-25 13:49:13 -0500

    Add navbar component

---
+++ app/assets/javascripts/components/navbar.es6.jsx
@@ -0,0 +1,42 @@
+class Navbar extends React.Component {
+  logOutButton () {
+    if (this.props.is_user_signed_in) {
+      return (
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link"
+               rel="nofollow"
+               data-method="delete"
+               href="/users/sign_out">
+              Log out
+            </a>
+          </li>
+        </ul>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <nav className="navbar fixed-top navbar-toggleable-md navbar-light bg-faded navbar-full">
+          <button className="navbar-toggler navbar-toggler-right"
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#navbar"
+                  aria-controls="navbar"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <a href="/" className="navbar-brand">Groceries</a>
+
+          <div className="collapse navbar-collapse" id="navbar">
+            { this.logOutButton() }
+          </div>
+        </nav>
+      </div>
+    )
+  }
+}
