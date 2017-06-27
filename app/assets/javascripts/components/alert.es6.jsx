aec115c48bb790e12bbd3acead7d52431ba759ed
maxvanbel <maxvanbel@gmail.com>
2017-06-26 21:47:43 -0500

    alert and notice combined in one component

---
+++ app/assets/javascripts/components/alert.es6.jsx
@@ -0,0 +1,12 @@
+class Alert extends React.Component{
+  render() {
+    return (
+      <div className={`alert alert-${ this.props.alert_class } alert-dismissible fade show`} role="alert">
+        <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+          {this.props.text }
+      </div>
+    )
+  }
+}
