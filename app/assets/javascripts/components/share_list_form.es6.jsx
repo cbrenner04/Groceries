37eecf2558763f5fb240663994855c0cdc3a05cb
Christopher Brenner <cbrenner265@gmail.com>
2017-06-26 21:56:31 -0500

    Update share list form to component (#8)

---
+++ app/assets/javascripts/components/share_list_form.es6.jsx
@@ -0,0 +1,60 @@
+class ShareListForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userId: '',
+      listId: this.props.list.id,
+      users: this.props.users
+    }
+  }
+
+  handleUserInput(event) {
+    const name = event.target.name;
+    const obj = {};
+    obj[name] = event.target.value;
+    this.setState(obj);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const users_list = {
+      user_id: this.state.userId,
+      list_id: this.state.listId
+    }
+    $.post(
+      `/users_lists?list_id=${this.props.list.id}`,
+      { users_list }
+    ).done(() => {
+      window.location = '/lists';
+    });
+  }
+
+  render() {
+    return(
+      <form onSubmit={ (event) => this.handleSubmit(event) }>
+        <div className="form-group">
+          <label htmlFor="usersListUserId">
+            Select who you would like to share this list with:
+          </label>
+          <select className="form-control"
+                  name="userId"
+                  id="usersListUserId"
+                  value={ this.state.value }
+                  onChange={ (event) => this.handleUserInput(event) }>
+            <option value="">Please select</option>
+            { this.props.users.map((user) => {
+              return (
+                <option key={ user.id } value={ user.id }>
+                  { user.first_name }
+                </option>
+              )
+            }) }
+          </select>
+        </div>
+        <input type="submit"
+               value="Share List"
+               className="btn btn-success btn-block action-button" />
+      </form>
+    )
+  }
+}
