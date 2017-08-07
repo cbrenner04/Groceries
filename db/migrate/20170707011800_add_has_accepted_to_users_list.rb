14adca121a9f45e1b6f431a336a95fd618d8467e
cbrenner04 <cbrenner265@gmail.com>
2017-08-07 12:52:56 -0500

    Fix migration to skip validation on users_list

--- db/migrate/20170707011800_add_has_accepted_to_users_list.rb
+++ db/migrate/20170707011800_add_has_accepted_to_users_list.rb
@@ -2,7 +2,10 @@ class AddHasAcceptedToUsersList < ActiveRecord::Migration[5.0]
   def change
     add_column :users_lists, :has_accepted, :boolean
 
-    UsersList.all.each { |list| list.update!(has_accepted: true) }
+    UsersList.all.each do |list|
+      list.has_accepted = true
+      list.save(validate: false)
+    end
 
     change_column :users_lists, :has_accepted, :boolean, null: false, default: false
   end
