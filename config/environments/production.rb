3a7c84fa0dcd806fb9cf1ae0be017b5980a72141
cbrenner04 <cbrenner265@gmail.com>
2017-08-07 14:19:48 -0500

    Update sendrid to mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,13 +88,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
+    port: ENV['MAILGUN_SMTP_PORT'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
     domain: 'cjb-groceries.heroku.com',
-    address: 'smtp.sendgrid.net',
-    port: 587,
     authentication: :plain,
-    enable_starttls_auto: true
   }
 
   # set default url for heroku
