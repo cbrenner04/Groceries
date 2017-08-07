4c409d1f9d7474b5ceef8c06261b8c17fc1753b5
cbrenner04 <cbrenner265@gmail.com>
2017-08-07 14:07:54 -0500

    Revert "Switch sengrid to gmail"
    This reverts commit dd85d1002b97f53a8257654246d3be952fa7134f.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,10 +88,10 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    user_name: ENV['GMAIL_USERNAME'],
-    password: ENV['GMAIL_PASSWORD'],
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
     domain: 'cjb-groceries.heroku.com',
-    address: 'smtp.gmail.com',
+    address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
     enable_starttls_auto: true
