2c0ff891dcbc5249a0f3dbabf783eaae67b5a0fd
Christopher Brenner <cbrenner265@gmail.com>
2017-08-20 12:32:51 -0500

    Add ability to invite a user without sharing a list (#19)
    [Finish #150383446]

--- spec/controllers/users/invitations_controller_spec.rb
+++ spec/controllers/users/invitations_controller_spec.rb
@@ -13,99 +13,105 @@ module Users
     end
 
     describe "POST #create" do
-      context "with valid params" do
-        it "creates a new user" do
-          expect do
-            post :create, params: {
-              user: {
-                email: "foo@bar.com"
-              },
-              list_id: list.id
-            }
-          end.to change(User, :count).by 1
-        end
-
-        it "creates a new users_list" do
-          expect do
-            post :create, params: {
-              user: {
-                email: "foo@bar.com"
-              },
-              list_id: list.id
-            }
-          end.to change(UsersList, :count).by 1
-        end
-      end
-
-      context "with invalid params" do
-        it "re-renders the 'new' template" do
-          post :create, params: {
-            user: {
-              email: nil
-            },
-            list_id: list.id
-          }
-
-          expect(response).to render_template("new")
-        end
-      end
-
-      describe "when user already exists" do
-        let(:other_user) { create :user }
-
-        context "when users_list already exists" do
-          before { create :users_list, user: other_user, list: list }
-
-          it "does not create a new users_list" do
+      context "when list_id param exists" do
+        context "with valid params" do
+          it "creates a new user" do
             expect do
               post :create, params: {
-                user: {
-                  email: other_user.email
-                },
+                user: { email: "foo@bar.com" },
                 list_id: list.id
               }
-            end.to_not change(UsersList, :count)
+            end.to change(User, :count).by 1
           end
 
-          it "redirects and gives correct alert" do
-            post :create, params: {
-              user: {
-                email: other_user.email
-              },
-              list_id: list.id
-            }
-            expect(response)
-              .to redirect_to new_users_list_path(list_id: list.id)
-            expect(flash[:notice]).to be_present
-            expect(flash[:notice])
-              .to eq "List already shared with #{other_user.email}"
-          end
-        end
-
-        context "when users_list does not already exist" do
           it "creates a new users_list" do
             expect do
               post :create, params: {
-                user: {
-                  email: other_user.email
-                },
+                user: { email: "foo@bar.com" },
                 list_id: list.id
               }
-            end.to change(UsersList, :count).by(1)
+            end.to change(UsersList, :count).by 1
           end
+        end
 
-          it "redirects and gives correct alert" do
+        context "with invalid params" do
+          it "re-renders the 'new' template" do
             post :create, params: {
-              user: {
-                email: other_user.email
-              },
+              user: { email: nil },
               list_id: list.id
             }
-            expect(response)
-              .to redirect_to new_users_list_path(list_id: list.id)
-            expect(flash[:notice]).to be_present
-            expect(flash[:notice])
-              .to eq "List has been shared with #{other_user.email}"
+
+            expect(response).to render_template("new")
+          end
+        end
+
+        describe "when user already exists" do
+          let(:other_user) { create :user }
+
+          context "when users_list already exists" do
+            before { create :users_list, user: other_user, list: list }
+
+            it "does not create a new users_list" do
+              expect do
+                post :create, params: {
+                  user: { email: other_user.email },
+                  list_id: list.id
+                }
+              end.to_not change(UsersList, :count)
+            end
+
+            it "redirects and gives correct alert" do
+              post :create, params: {
+                user: { email: other_user.email },
+                list_id: list.id
+              }
+              expect(response)
+                .to redirect_to new_users_list_path(list_id: list.id)
+              expect(flash[:notice]).to be_present
+              expect(flash[:notice])
+                .to eq "List already shared with #{other_user.email}"
+            end
+          end
+
+          context "when users_list does not already exist" do
+            it "creates a new users_list" do
+              expect do
+                post :create, params: {
+                  user: { email: other_user.email },
+                  list_id: list.id
+                }
+              end.to change(UsersList, :count).by(1)
+            end
+
+            it "redirects and gives correct alert" do
+              post :create, params: {
+                user: { email: other_user.email },
+                list_id: list.id
+              }
+              expect(response)
+                .to redirect_to new_users_list_path(list_id: list.id)
+              expect(flash[:notice]).to be_present
+              expect(flash[:notice])
+                .to eq "List has been shared with #{other_user.email}"
+            end
+          end
+        end
+      end
+
+      context "when list_id param does not exist" do
+        context "with valid params" do
+          it "creates a new user" do
+            expect do
+              post :create, params: { user: { email: "foo@bar.com" } }
+            end.to change(User, :count).by 1
+          end
+        end
+
+        context "with invalid params" do
+          it "re-renders the 'new' template" do
+            post :create, params: { user: { email: nil } }
+
+            expect(response).to render_template("new")
           end
         end
       end
