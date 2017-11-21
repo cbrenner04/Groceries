af49ac5044afd854451aada560e53ef2fccdb02f
Christopher Brenner <cbrenner265@gmail.com>
2017-11-20 22:43:10 -0600

    Add ability to read/unread book list items (#34)

--- client/app/bundles/Groceries/components/PurchasedListItems.jsx
+++ client/app/bundles/Groceries/components/PurchasedListItems.jsx
@@ -22,6 +22,8 @@ export default class PurchasedListItems extends Component {
     listType: PropTypes.string.isRequired,
     handleItemUnPurchase: PropTypes.func.isRequired,
     onItemDelete: PropTypes.func.isRequired,
+    onItemRead: PropTypes.func.isRequired,
+    onItemUnRead: PropTypes.func.isRequired,
     listUsers: PropTypes.arrayOf(
       PropTypes.shape({
         id: PropTypes.number.isRequired,
@@ -43,10 +45,25 @@ export default class PurchasedListItems extends Component {
     this.props.onItemDelete(item);
   }
 
+  readItem = (item) => {
+    this.props.onItemRead(item);
+  }
+
+  unReadItem = (item) => {
+    this.props.onItemUnRead(item);
+  }
+
+  title = () => {
+    if (this.props.listType === 'ToDoList') {
+      return 'Completed';
+    }
+    return 'Purchased';
+  }
+
   render() {
     return (
       <div>
-        <h2>Purchased</h2>
+        <h2>{this.title()}</h2>
         <ul className="list-group">
           {
             this.props.items.map(item => (
@@ -55,6 +72,8 @@ export default class PurchasedListItems extends Component {
                 key={item.id}
                 unPurchaseItem={this.handleUnPurchase}
                 handleItemDelete={this.deleteItem}
+                handleItemRead={this.readItem}
+                handleItemUnRead={this.unReadItem}
                 listType={this.props.listType}
                 listUsers={this.props.listUsers}
               />
