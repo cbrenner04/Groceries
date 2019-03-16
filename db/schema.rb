9ef72b17cc840329f69626f8a6366a9d504c59d1
Christopher Brenner <cbrenner265@gmail.com>
2019-03-16 10:34:24 -0500

    Fix vulnerabilities (#64)
    * Fix vulnerabilities
    
    * Fuck you rubocop
    
    * Update devise
    
    * Fuck you eslint

--- db/schema.rb
+++ db/schema.rb
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 2018_08_05_212618) do
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invitations_count"], name: "index_users_on_invitations_count"
     t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
+    t.index ["invited_by_type", "invited_by_id"], name: "index_users_on_invited_by_type_and_invited_by_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
