8ebe459186a7f63f51f6b7b8cdafec4dc167ed39
cbrenner04 <cbrenner265@gmail.com>
2020-01-22 06:12:07 -0600

    lint

--- app/models/book_list.rb
+++ app/models/book_list.rb
@@ -5,12 +5,14 @@ class BookList < List
   has_many :book_list_items, dependent: :destroy
 
   def categories
-    book_list_items.map(&:category).concat([
-      "Action & adventure", "Autobiography", "Biography", "Crime", "Drama",
-      "Fantasy", "Graphic novel", "Health", "Historical fiction", "History",
-      "Horror", "Memoir", "Mystery", "Poetry", "Science", "Science fiction",
-      "Self help", "Spirituality", "Textbook", "Thriller", "Thriller", "Travel",
-      "True crime",
-    ]).uniq.compact.reject(&:empty?).sort
+    book_list_items.map(&:category).concat(
+      [
+        "Action & adventure", "Autobiography", "Biography", "Crime", "Drama",
+        "Fantasy", "Graphic novel", "Health", "Historical fiction", "History",
+        "Horror", "Memoir", "Mystery", "Poetry", "Science", "Science fiction",
+        "Self help", "Spirituality", "Textbook", "Thriller", "Thriller",
+        "Travel", "True crime"
+      ]
+    ).uniq.compact.reject(&:empty?).sort
   end
 end
--- app/models/grocery_list.rb
+++ app/models/grocery_list.rb
@@ -5,12 +5,14 @@ class GroceryList < List
   has_many :grocery_list_items, dependent: :destroy
 
   def categories
-    grocery_list_items.map(&:category).concat([
-      "Alcohol", "Baby", "Bakery", "Baking", "Beverages",
-      "Canned Goods & Soups", "Cereal", "Cleaning Supplies", "Condiments",
-      "Dairy", "Deli", "Flowers", "Frozen Foods", "Grains, Pasta & Sides",
-      "Health & Beauty", "International", "Meat & Seafood", "Paper Products",
-      "Pet", "Pharmacy", "Produce", "Snacks", "Spices"
-    ]).uniq.compact.reject(&:empty?).sort
+    grocery_list_items.map(&:category).concat(
+      [
+        "Alcohol", "Baby", "Bakery", "Baking", "Beverages",
+        "Canned Goods & Soups", "Cereal", "Cleaning Supplies", "Condiments",
+        "Dairy", "Deli", "Flowers", "Frozen Foods", "Grains, Pasta & Sides",
+        "Health & Beauty", "International", "Meat & Seafood", "Paper Products",
+        "Pet", "Pharmacy", "Produce", "Snacks", "Spices"
+      ]
+    ).uniq.compact.reject(&:empty?).sort
   end
 end
