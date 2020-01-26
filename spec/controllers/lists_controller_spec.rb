f1e2c6d9f5a6c8b8337d7f4093631da9a31a4082
cbrenner04 <cbrenner265@gmail.com>
2020-01-26 13:56:03 -0600

    Fix refresh list so it keeps item categories [#170913352]

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -517,7 +517,7 @@ RSpec.describe ListsController do
     context "when user is owner" do
       describe "when old list is a BookList" do
         it "creates new list" do
-          list = BookList.create!(name: "NewBookList", owner: user)
+          list = BookList.create!(name: "NewBookList", owner: user, completed: true)
           expect do
             post :refresh_list, params: {
               id: list.id
@@ -526,23 +526,27 @@ RSpec.describe ListsController do
         end
 
         it "creates new items" do
-          list = BookList.create!(name: "NewBookList", owner: user)
+          list = BookList.create!(name: "NewBookList", owner: user, completed: true)
           BookListItem.create!(
             user: user,
             book_list: list,
-            title: "foo"
+            title: "foo",
+            category: "foo"
           )
           expect do
             post :refresh_list, params: {
               id: list.id
             }
           end.to change(BookListItem, :count).by 1
+          new_list_item = BookListItem.last
+          expect(new_list_item[:title]).to eq "foo"
+          expect(new_list_item[:category]).to eq "foo"
         end
       end
 
       describe "when old list is a GroceryList" do
         it "creates new list" do
-          list = GroceryList.create!(name: "NewGroceryList", owner: user)
+          list = GroceryList.create!(name: "NewGroceryList", owner: user, completed: true)
           expect do
             post :refresh_list, params: {
               id: list.id
@@ -551,24 +555,28 @@ RSpec.describe ListsController do
         end
 
         it "creates new items" do
-          list = GroceryList.create!(name: "NewGroceryList", owner: user)
+          list = GroceryList.create!(name: "NewGroceryList", owner: user, completed: true)
           GroceryListItem.create!(
             user: user,
             grocery_list: list,
             product: "foo",
-            quantity: 1
+            quantity: 1,
+            category: "foo"
           )
           expect do
             post :refresh_list, params: {
               id: list.id
             }
           end.to change(GroceryListItem, :count).by 1
+          new_list_item = GroceryListItem.last
+          expect(new_list_item[:product]).to eq "foo"
+          expect(new_list_item[:category]).to eq "foo"
         end
       end
 
-      describe "when old list is a BookList" do
+      describe "when old list is a MusicList" do
         it "creates new list" do
-          list = MusicList.create!(name: "NewMusicList", owner: user)
+          list = MusicList.create!(name: "NewMusicList", owner: user, completed: true)
           expect do
             post :refresh_list, params: {
               id: list.id
@@ -577,23 +585,27 @@ RSpec.describe ListsController do
         end
 
         it "creates new items" do
-          list = MusicList.create!(name: "NewMusicList", owner: user)
+          list = MusicList.create!(name: "NewMusicList", owner: user, completed: true)
           MusicListItem.create!(
             user: user,
             music_list: list,
-            title: "foo"
+            title: "foo",
+            category: "foo"
           )
           expect do
             post :refresh_list, params: {
               id: list.id
             }
           end.to change(MusicListItem, :count).by 1
+          new_list_item = MusicListItem.last
+          expect(new_list_item[:title]).to eq "foo"
+          expect(new_list_item[:category]).to eq "foo"
         end
       end
 
-      describe "when old list is a GroceryList" do
+      describe "when old list is a ToDoList" do
         it "creates new list" do
-          list = ToDoList.create!(name: "NewToDoList", owner: user)
+          list = ToDoList.create!(name: "NewToDoList", owner: user, completed: true)
           expect do
             post :refresh_list, params: {
               id: list.id
@@ -602,17 +614,21 @@ RSpec.describe ListsController do
         end
 
         it "creates new items" do
-          list = ToDoList.create!(name: "NewToDoList", owner: user)
+          list = ToDoList.create!(name: "NewToDoList", owner: user, completed: true)
           ToDoListItem.create!(
             user: user,
             to_do_list: list,
-            task: "foo"
+            task: "foo",
+            category: "foo"
           )
           expect do
             post :refresh_list, params: {
               id: list.id
             }
           end.to change(ToDoListItem, :count).by 1
+          new_list_item = ToDoListItem.last
+          expect(new_list_item[:task]).to eq "foo"
+          expect(new_list_item[:category]).to eq "foo"
         end
       end
     end
