920964586b432ba25103d5c23d909ca4b30d57cd
cbrenner04 <cbrenner265@gmail.com>
2020-02-09 11:34:30 -0600

    Update list

--- client/app/bundles/Groceries/components/List.jsx
+++ client/app/bundles/Groceries/components/List.jsx
@@ -1,108 +1,70 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import { formatDate } from '../utils/format';
 import listIconClass from '../utils/list_icon';
 
-export default class List extends Component {
-  static propTypes = {
-    userId: PropTypes.number.isRequired,
-    list: PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-      type: PropTypes.string.isRequired,
-      created_at: PropTypes.string.isRequired,
-      completed: PropTypes.bool.isRequired,
-      users_list_id: PropTypes.number,
-      owner_id: PropTypes.number,
-      refreshed: PropTypes.bool,
-    }).isRequired,
-    accepted: PropTypes.bool,
-    onListDeletion: PropTypes.func,
-    onListCompletion: PropTypes.func,
-    onListRefresh: PropTypes.func,
-    onListAcceptance: PropTypes.func,
-    onListRejection: PropTypes.func,
-  }
+function List(props) {
+  const [currentUserPermissions, setCurrentUserPermissions] = useState('read');
+  const { list, userId } = props;
 
-  static defaultProps = {
-    onListDeletion: null,
-    onListCompletion: null,
-    onListRefresh: null,
-    accepted: false,
-    onListAcceptance: null,
-    onListRejection: null,
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentUserPermissions: 'read',
-    };
-  }
-
-  componentDidMount() {
-    const { list } = this.props;
+  useEffect(() => {
     $.ajax({
       type: 'GET',
       url: `/lists/${list.id}/users_lists/${list.users_list_id}`,
       dataType: 'JSON',
-    }).done(({ permissions: currentUserPermissions }) => {
-      this.setState({ currentUserPermissions });
+    }).done(({ permissions }) => {
+      setCurrentUserPermissions(permissions);
     });
-  }
+  });
 
-  handleDelete = () => this.props.onListDeletion(this.props.list);
+  const handleDelete = () => props.onListDeletion(list);
+  const handleComplete = () => props.onListCompletion(list);
+  const handleRefresh = () => props.onListRefresh(list);
+  const handleAccept = () => props.onListAcceptance(list);
+  const handleReject = () => props.onListRejection(list);
 
-  handleComplete = () => this.props.onListCompletion(this.props.list);
-
-  handleRefresh = () => this.props.onListRefresh(this.props.list);
-
-  handleAccept = () => this.props.onListAcceptance(this.props.list);
-
-  handleReject = () => this.props.onListRejection(this.props.list);
-
-  incompleteListButtons = () => (
+  const incompleteListButtons = () => (
     <div className="btn-group float-right" role="group">
       <button
-        onClick={this.handleComplete}
+        onClick={handleComplete}
         className="btn btn-link p-0 mr-3"
-        disabled={this.props.userId !== this.props.list.owner_id}
-        style={{ opacity: this.props.userId !== this.props.list.owner_id ? 0.3 : 1 }}
+        disabled={userId !== list.owner_id}
+        style={{ opacity: userId !== list.owner_id ? 0.3 : 1 }}
         data-test-id="incomplete-list-complete"
       >
         <i className="fa fa-check-square-o fa-2x text-success" />
       </button>
       <Link
-        to={`lists/${this.props.list.id}/users_lists`}
+        to={`lists/${list.id}/users_lists`}
         className="btn btn-link p-0 mr-3"
-        disabled={this.state.currentUserPermissions !== 'write'}
+        disabled={currentUserPermissions !== 'write'}
         style={{
-          pointerEvents: this.state.currentUserPermissions !== 'write' ? 'none' : 'auto',
-          opacity: this.state.currentUserPermissions !== 'write' ? 0.3 : 1,
+          pointerEvents: currentUserPermissions !== 'write' ? 'none' : 'auto',
+          opacity: currentUserPermissions !== 'write' ? 0.3 : 1,
         }}
         data-test-id="incomplete-list-share"
       >
         <i className="fa fa-users fa-2x text-primary" />
       </Link>
       <Link
-        to={`/lists/${this.props.list.id}/edit`}
+        to={`/lists/${list.id}/edit`}
         className="btn btn-link p-0 mr-3"
-        disabled={this.props.userId !== this.props.list.owner_id}
+        disabled={userId !== list.owner_id}
         style={{
-          pointerEvents: this.props.userId !== this.props.list.owner_id ? 'none' : 'auto',
-          opacity: this.props.userId !== this.props.list.owner_id ? 0.3 : 1,
+          pointerEvents: userId !== list.owner_id ? 'none' : 'auto',
+          opacity: userId !== list.owner_id ? 0.3 : 1,
         }}
         data-test-id="incomplete-list-edit"
       >
         <i className="fa fa-pencil-square-o fa-2x text-warning" />
       </Link>
       <button
-        onClick={this.handleDelete}
+        onClick={handleDelete}
         className="btn btn-link p-0"
-        disabled={this.props.userId !== this.props.list.owner_id}
-        style={{ opacity: this.props.userId !== this.props.list.owner_id ? 0.3 : 1 }}
+        disabled={userId !== list.owner_id}
+        style={{ opacity: userId !== list.owner_id ? 0.3 : 1 }}
         data-test-id="incomplete-list-trash"
       >
         <i className="fa fa-trash fa-2x text-danger" />
@@ -110,22 +72,22 @@ export default class List extends Component {
     </div>
   );
 
-  completedListButtons = () => (
+  const completedListButtons = () => (
     <div className="btn-group float-right" role="group">
       <button
-        onClick={this.handleRefresh}
+        onClick={handleRefresh}
         className="btn btn-link p-0 mr-3"
-        disabled={this.props.userId !== this.props.list.owner_id}
-        style={{ opacity: this.props.userId !== this.props.list.owner_id ? 0.3 : 1 }}
+        disabled={userId !== list.owner_id}
+        style={{ opacity: userId !== list.owner_id ? 0.3 : 1 }}
         data-test-id="complete-list-refresh"
       >
         <i className="fa fa-refresh fa-2x text-primary" />
       </button>
       <button
-        onClick={this.handleDelete}
+        onClick={handleDelete}
         className="btn btn-link p-0"
-        disabled={this.props.userId !== this.props.list.owner_id}
-        style={{ opacity: this.props.userId !== this.props.list.owner_id ? 0.3 : 1 }}
+        disabled={userId !== list.owner_id}
+        style={{ opacity: userId !== list.owner_id ? 0.3 : 1 }}
         data-test-id="complete-list-trash"
       >
         <i className="fa fa-trash fa-2x text-danger" />
@@ -133,55 +95,83 @@ export default class List extends Component {
     </div>
   );
 
-  pendingListButtons = () => (
+  const pendingListButtons = () => (
     <div className="btn-group float-right" role="group">
-      <button onClick={this.handleAccept} className="btn btn-link p-0 mr-3" data-test-id="pending-list-accept">
+      <button onClick={handleAccept} className="btn btn-link p-0 mr-3" data-test-id="pending-list-accept">
         <i className="fa fa-check-square-o fa-2x text-success" />
       </button>
-      <button onClick={this.handleReject} className="btn btn-link p-0 mr-3" data-test-id="pending-list-trash">
+      <button onClick={handleReject} className="btn btn-link p-0 mr-3" data-test-id="pending-list-trash">
         <i className="fa fa-trash fa-2x text-danger" />
       </button>
     </div>
   );
 
-  acceptedListButtons = () => (this.props.list.completed ? this.completedListButtons() : this.incompleteListButtons());
+  const acceptedListButtons = () => (list.completed ? completedListButtons() : incompleteListButtons());
+  const acceptedListTestClass = () => (list.completed ? 'completed-list' : 'non-completed-list');
 
-  acceptedListTestClass = () => (this.props.list.completed ? 'completed-list' : 'non-completed-list');
-
-  listTitle = () => (
+  const listTitle = () => (
     <h5 className="mb-1">
-      <i className={`fa ${listIconClass(this.props.list.type)} text-info mr-3`} />
-      {this.props.list.name}{this.props.list.refreshed && '*'}
+      <i className={`fa ${listIconClass(list.type)} text-info mr-3`} />
+      {list.name}{list.refreshed && '*'}
     </h5>
   );
 
-  acceptedListLink = () => (
-    <Link to={`/lists/${this.props.list.id}`} className="router-link">
-      {this.listTitle()}
+  const acceptedListLink = () => (
+    <Link to={`/lists/${list.id}`} className="router-link">
+      {listTitle()}
     </Link>
   );
 
-  render() {
-    return (
-      <div
-        className={`list-group-item ${this.props.accepted ? 'accepted-list' : 'pending-list'}`}
-        style={{ display: 'block' }}
-        data-test-class={this.props.accepted ? this.acceptedListTestClass() : 'pending-list'}
-      >
-        <div className="row">
-          <div className="col-md-6 pt-1">
-            {this.props.accepted ? this.acceptedListLink() : this.listTitle()}
-          </div>
-          <div className="col-md-4 pt-1">
-            <small className="text-muted">
-              {formatDate(this.props.list.created_at)}
-            </small>
-          </div>
-          <div className="col-md-2">
-            {this.props.accepted ? this.acceptedListButtons() : this.pendingListButtons()}
-          </div>
+  return (
+    <div
+      className={`list-group-item ${props.accepted ? 'accepted-list' : 'pending-list'}`}
+      style={{ display: 'block' }}
+      data-test-class={props.accepted ? acceptedListTestClass() : 'pending-list'}
+    >
+      <div className="row">
+        <div className="col-md-6 pt-1">
+          {props.accepted ? acceptedListLink() : listTitle()}
+        </div>
+        <div className="col-md-4 pt-1">
+          <small className="text-muted">
+            {formatDate(list.created_at)}
+          </small>
+        </div>
+        <div className="col-md-2">
+          {props.accepted ? acceptedListButtons() : pendingListButtons()}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+List.propTypes = {
+  userId: PropTypes.number.isRequired,
+  list: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    created_at: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired,
+    users_list_id: PropTypes.number,
+    owner_id: PropTypes.number,
+    refreshed: PropTypes.bool,
+  }).isRequired,
+  accepted: PropTypes.bool,
+  onListDeletion: PropTypes.func,
+  onListCompletion: PropTypes.func,
+  onListRefresh: PropTypes.func,
+  onListAcceptance: PropTypes.func,
+  onListRejection: PropTypes.func,
+};
+
+List.defaultProps = {
+  onListDeletion: null,
+  onListCompletion: null,
+  onListRefresh: null,
+  accepted: false,
+  onListAcceptance: null,
+  onListRejection: null,
+};
+
+export default List;
